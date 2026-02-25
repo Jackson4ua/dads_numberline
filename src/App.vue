e63481fac95f1561ca3f7c2710f60018f598868d
Jackson4ua <jackson4ua@gmail.com>
2026-02-25 00:30:24 -0600

    fix: update features

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,64 @@
 <script setup lang="ts">
-const numbersList: Ref<number[]> = []
+import { ref } from 'vue'
+
+const numbersList = ref<number[]>([])
+const mysteryNum = ref(0)
 
 for (let i = 1; i <= 100; i++) {
-  numbersList.push(i)
+  numbersList.value.push(i)
 }
 
-function revealNumber(number: any) {
-  document
-    .getElementById(`${number}-btn`)!
-    .style
-    .setProperty('visibility', 'visible')
-  setTimeout(() => {
-    document
-      .getElementById(`${number}-btn`)!
-      .style
-      .setProperty('visibility', 'hidden')
-  }, 1500)
+function newNumber() {
+  mysteryNum.value = Math.round(Math.random() * 100)
+  for (let i = 1; i <= 100; i++) {
+    const item = document.getElementById(`${i}-btn`)
+    if (item) {
+      const text = document.getElementById(`${i}-text`)
+      text?.style.setProperty('visibility', 'hidden')
+      if (item!.classList.contains('btn-success')) {
+        item!.classList.remove('btn-success')
+      }
+      if (item!.classList.contains('btn-outline-danger')) {
+        item!.classList.remove('btn-outline-danger')
+      }
+      item!.classList.add('btn-outline-primary')
+    }
+  }
 }
+
+function guessNumber(guess) {
+  if (guess === mysteryNum.value) {
+    document
+      .getElementById(`${guess}-btn`)!
+      .classList
+      .remove('btn-outline-primary')
+
+    document.getElementById(`${guess}-btn`)!.classList.add('btn-success')
+
+    document
+      .getElementById(`${guess}-text`)!
+      .style
+      .setProperty('visibility', 'visible')
+  }
+  else {
+    document
+      .getElementById(`${guess}-btn`)!
+      .classList
+      .remove('btn-outline-primary')
+
+    document
+      .getElementById(`${guess}-btn`)!
+      .classList
+      .add('btn-outline-danger')
+
+    document
+      .getElementById(`${guess}-text`)!
+      .style
+      .setProperty('visibility', 'visible')
+  }
+}
+
+newNumber()
 </script>
 
 <template>
@@ -25,6 +67,14 @@ function revealNumber(number: any) {
       <h2 class="display-2">
         Mr. Holbrook's Number Line
       </h2>
+      <div class="d-flex justify-content-evenly">
+        <h2 class="display-5">
+          Find: {{ mysteryNum }}
+        </h2>
+        <button class="btn btn-primary" @click="newNumber()">
+          New Number
+        </button>
+      </div>
     </div>
     <div
       class="d-flex flex-wrap justify-content-center align-items-center border rounded"
@@ -37,11 +87,12 @@ function revealNumber(number: any) {
         style="width: 10%"
       >
         <button
+          :id="`${number}-btn`"
           class="btn btn-outline-primary m-2"
           style="width: -webkit-fill-available"
-          @click="revealNumber(number)"
+          @click="guessNumber(number)"
         >
-          <div :id="`${number}-btn`" style="visibility: hidden">
+          <div :id="`${number}-text`" style="visibility: hidden">
             {{ number }}
           </div>
         </button>
